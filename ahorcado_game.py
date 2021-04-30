9fe9231eda8be815ebd1e83eb597136dcdef1f2a
Wilson Montenegro <wilmontenegro@me.com>
2021-04-29 21:57:32 -0500

    Imprimiendo las letras correctas solo 1

--- ahorcado_game.py
+++ ahorcado_game.py
@@ -15,18 +15,23 @@ def read():
 
 #Solicitando letras
 def input_characters():
+    global character_user
     character_user = input("Ingresa una letra: ")
     return character_user
 
 #Comparación
 random_word_characters = []
+rigth_word_characters = []
 def compare():
     for character in random_word:
         random_word_characters.append(character)
+        rigth_word_characters.append("-")
 
     for character in random_word_characters:
         if input_characters() in random_word_characters:
-            print(character)
+            position_character = random_word_characters.index(character_user)
+            rigth_word_characters[position_character] = character_user
+            print(rigth_word_characters)
     print(random_word_characters)
 
 
