8563d04ac3fb19cce0f268adf2320238e47f8216
Wilson Montenegro <wilmontenegro@me.com>
2021-05-01 14:21:26 -0500

    Solucion 1 Will

--- ahorcado_game.py
+++ ahorcado_game.py
@@ -9,31 +9,37 @@ def read():
         for line in f:
             words.append(line)
 
-    global random_word
+    #Seleccionado Palabra
+    global random_word_uper
     random_word = words[random.randint(0,len(words)-1)]
-    print(random_word)
+    traslate_vocals = random_word.maketrans('áéíóú\n', 'aeiou ')
+    random_word_uper = random_word.translate(traslate_vocals).upper().strip()
+    print(random_word_uper)
+    print(len(random_word_uper))
     return random_word
     
 
-#Solicitando letras
+#Solicitando letras al jugador
 def input_characters():
     global character_user
-    character_user = input("Ingresa una letra: ")
+    character_user = input("Ingresa una letra: ").upper()
     return character_user
 
 random_word_characters = []
 rigth_word_characters = []
-print_rigth_word_characters = []
+#print_rigth_word_characters = []
+
 
 def words_to_play():
-    for character in random_word:
+    for character in random_word_uper:
         random_word_characters.append(character)
         rigth_word_characters.append("-")
-    rigth_word_characters.pop()
     
 
 #Comparación
+
 def compare():
+    global print_rigth_word_characters
     if input_characters() in random_word_characters:
         position_characters = list(enumerate(random_word_characters, 0))
         guess_characters = [(x) for x,i in position_characters if i == character_user]
@@ -43,23 +49,25 @@ def compare():
             rigth_word_characters[x] = character_user
             print_rigth_word_characters = reduce(lambda a,b: a + b, rigth_word_characters)
             os.system("clear")
-            print(print_rigth_word_characters)
-        print(random_word_characters)
+            print(print_rigth_word_characters)     
+
     else:
         os.system("clear")
         print_rigth_word_characters = reduce(lambda a,b: a + b, rigth_word_characters)
         print(print_rigth_word_characters)
-        
-        
+    
 
 def chances():
+    lives_value = 10
     words_to_play()
-    for _ in range(7):
+    for _ in range(lives_value):
         print("¡Adivina la Palabra!")
+        print('💚 '*lives_value)
+        lives_value -= 1
         compare()
-        
-        
-
+        if random_word_uper == print_rigth_word_characters:
+            print("GANASTE")
+            break
 
 def run():
     os.system("clear")
