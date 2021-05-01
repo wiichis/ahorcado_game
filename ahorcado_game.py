2576a6d7e6749d30e4aa8b8836e301e5c4bd56dc
Wilson Montenegro <wilmontenegro@me.com>
2021-05-01 01:05:00 -0500

    Actualizacion de pantalla ok

--- ahorcado_game.py
+++ ahorcado_game.py
@@ -21,45 +21,50 @@ def input_characters():
     character_user = input("Ingresa una letra: ")
     return character_user
 
-
-def lines()
-
-
 random_word_characters = []
 rigth_word_characters = []
+print_rigth_word_characters = []
 
-
-#Comparación
-def compare():
+def words_to_play():
     for character in random_word:
         random_word_characters.append(character)
         rigth_word_characters.append("-")
     rigth_word_characters.pop()
+    
 
-    # 7 Oportunidades para adivinar
-    for character in range(7):
-        if input_characters() in random_word_characters:
-            position_characters = list(enumerate(random_word_characters, 0))
-            guess_characters = [(x) for x,i in position_characters if i == character_user]
+#Comparación
+def compare():
+    if input_characters() in random_word_characters:
+        position_characters = list(enumerate(random_word_characters, 0))
+        guess_characters = [(x) for x,i in position_characters if i == character_user]
             
-            #Llenando las letras correctas
-            for x in guess_characters:
-                rigth_word_characters[x] = character_user
-                print_rigth_word_characters = reduce(lambda a,b: a + b, rigth_word_characters)
-                os.system("clear")
+        #Llenando las letras correctas
+        for x in guess_characters:
+            rigth_word_characters[x] = character_user
+            print_rigth_word_characters = reduce(lambda a,b: a + b, rigth_word_characters)
+            os.system("clear")
             print(print_rigth_word_characters)
-    print(random_word_characters)
+        print(random_word_characters)
+    else:
+        os.system("clear")
+        print_rigth_word_characters = reduce(lambda a,b: a + b, rigth_word_characters)
+        print(print_rigth_word_characters)
+        
+        
 
 def chances():
-    for chance in range(7):
-        os.system("clear")
+    words_to_play()
+    for _ in range(7):
         print("¡Adivina la Palabra!")
+        compare()
+        
+        
 
 
 def run():
-    chances()
+    os.system("clear")
     read()
-    compare()
+    chances()
 
 if __name__=='__main__':
 
